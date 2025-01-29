ab75a1b992bd0afbc5a4eb8cc2a550b7caf9dba1
Veljko03 <veljkodovragovic@gmail.com>
2025-01-29 13:34:50 +0100

    added socket io diplayed message on connect and connected it to front using cors

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require("express");
 const { createServer } = require("node:http");
 const cors = require("cors");
 const { roomRouter } = require("./routes");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/", roomRouter);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
